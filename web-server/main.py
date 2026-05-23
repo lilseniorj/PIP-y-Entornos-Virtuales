429a2463292bae5591e14e04c437909c81790809
lilseniorj <jesusvarguer18@gmail.com>
2026-05-23 18:35:09 -0500

    Dockerizando scripts de Python

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,24 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3,]
+
+@app.get("/categories")
+def get_categories():
+    return store.get_categories()
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Contact</h1>
+        <p>This is the contact page.</p>
+    """
+
 
 def run():
     store.get_categories()
--- web-server/store.py
+++ web-server/store.py
@@ -2,6 +2,4 @@ import requests
 
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    print(r.status_code)
-    print(r.text)
-    print(type(r.text))
+    return r.json()
