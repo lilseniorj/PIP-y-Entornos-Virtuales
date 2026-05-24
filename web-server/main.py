1e753b33799901e523adf6b35303cc8d81ff95a5
lilseniorj <jesusvarguer18@gmail.com>
2026-05-23 19:02:32 -0500

    Automatizando la vinculacion de archivos

--- web-server/main.py
+++ web-server/main.py
@@ -16,7 +16,9 @@ def get_categories():
 def get_contact():
     return """
         <h1>Contact</h1>
-        <p>This is the contact page.</p>
+        <p>This is the contact page.
+            lilseniorj
+        </p>
     """
 
 
